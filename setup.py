579bdf1e9744fcafb9507b9d3a9d95915627f156
Enrico Trombetta <trombetta.enricom@protonmail.com>
2026-06-11 14:50:25 +0900

    Modernize project a bit, add pyproject for uv

--- setup.py
+++ setup.py
@@ -12,12 +12,14 @@ except ImportError:
 else:
     use_cython = True
 
+
 class build_ext(_build_ext):
     # see http://stackoverflow.com/q/19919905 for explanation
     def finalize_options(self):
         _build_ext.finalize_options(self)
-        __builtins__.__NUMPY_SETUP__ = False
+        # __builtins__.__NUMPY_SETUP__ = False
         import numpy as np
+
         self.include_dirs.append(np.get_include())
 
     # if extension modules fail to build, keep going anyway
@@ -25,49 +27,66 @@ class build_ext(_build_ext):
         try:
             _build_ext.run(self)
         except CompileError:
-            warn('Failed to build extension modules')
+            warn("Failed to build extension modules")
+
 
 class sdist(_sdist):
     def run(self):
         try:
             from Cython.Build import cythonize
-            cythonize(os.path.join('pybasicbayes','**','*.pyx'))
+
+            cythonize(os.path.join("pybasicbayes", "**", "*.pyx"))
         except:
-            warn('Failed to generate extension files from Cython sources')
+            warn("Failed to generate extension files from Cython sources")
         finally:
             _sdist.run(self)
 
-ext_modules=[
+
+ext_modules = [
     Extension(
-        'pybasicbayes.util.cstats', ['pybasicbayes/util/cstats.c'],
-        extra_compile_args=['-O3','-w']),
+        "pybasicbayes.util.cstats",
+        ["pybasicbayes/util/cstats.c"],
+        extra_compile_args=["-O3", "-w"],
+    ),
 ]
 
 if use_cython:
     from Cython.Build import cythonize
-    try:
-        ext_modules = cythonize(os.path.join('pybasicbayes','**','*.pyx'))
-    except:
-        warn('Failed to generate extension module code from Cython files')
 
-setup(name='pybasicbayes',
-      version='0.2.4',
-      description="Basic utilities for Bayesian inference",
-      author='Matthew James Johnson',
-      author_email='mattjj@csail.mit.edu',
-      url="http://github.com/mattjj/pybasicbayes",
-      packages=[
-          'pybasicbayes', 'pybasicbayes.distributions',
-          'pybasicbayes.util', 'pybasicbayes.testing', 'pybasicbayes.models'],
-      platforms='ALL',
-      keywords=[
-          'bayesian', 'inference', 'mcmc', 'variational inference',
-          'mean field', 'vb'],
-      install_requires=["numpy", "scipy", "matplotlib", "nose", "future"],
-      setup_requires=['numpy'],
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Programming Language :: Python',
-      ],
-      ext_modules=ext_modules,
-      cmdclass={'build_ext': build_ext, 'sdist': sdist})
+    try:
+        ext_modules = cythonize(os.path.join("pybasicbayes", "**", "*.pyx"))
+    except:
+        warn("Failed to generate extension module code from Cython files")
+
+setup(
+    name="pybasicbayes",
+    version="0.2.4",
+    description="Basic utilities for Bayesian inference",
+    author="Matthew James Johnson",
+    author_email="mattjj@csail.mit.edu",
+    url="http://github.com/mattjj/pybasicbayes",
+    packages=[
+        "pybasicbayes",
+        "pybasicbayes.distributions",
+        "pybasicbayes.util",
+        "pybasicbayes.testing",
+        "pybasicbayes.models",
+    ],
+    platforms="ALL",
+    keywords=[
+        "bayesian",
+        "inference",
+        "mcmc",
+        "variational inference",
+        "mean field",
+        "vb",
+    ],
+    install_requires=["numpy", "scipy", "matplotlib", "nose", "future"],
+    setup_requires=["numpy"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+    ],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext, "sdist": sdist},
+)
